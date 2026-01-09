9d82de4da6623d3e26f965683522a114b25568d0
Priya Patel <148117045+priya21005@users.noreply.github.com>
2026-01-09 13:06:33 +0530

    Add files via upload

---
+++ Skytus assessment-1.py
@@ -0,0 +1,74 @@
+#WAP TO print your name,age,city in line
+# name = "Priya"
+# age = 21
+# city = "Chikhali"
+
+# print(f"Name: {name}, Age: {age}, City: {city}")
+
+
+#Take a user input for two numbers and print their sum
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+
+# sum = num1 + num
+# print("Sum =", sum)
+
+
+#WAP to convert temperature from celsius to feranhit
+# celsius = float(input("Enter temperature in Celsius: "))
+
+# fahrenheit = (celsius * 9/5) + 32
+# print("Temperature in Fahrenheit =", fahrenheit)
+
+
+#store your name in variable and print in uppercase
+# my_name = "Priya"
+# print(my_name.upper())
+
+
+#Ask the user for their birth year and calculate  their current age
+# from datetime import date
+
+# birth_year = int(input("Enter your birth year: "))
+# current_year = date.today().year
+# age = current_year - birth_year
+
+# print("Your current age is", age)
+
+
+
+#WAP TO SWAP TO VARIABLE 
+# a = 25
+# b = 58
+
+# a, b = b, a
+# print("After swapping:", a, b)
+
+
+#Create a programme to calculate the area of rectangle from user
+ 
+# length = float(input("enter the length:"))
+# width = float(input("enter the width:"))
+
+# area_of_rectangle = length * width
+
+# print("area of rectangle:",area_of_rectangle )
+
+#WAP TO CHECK IF NUMBER IS POSITIVE OR NEGATIVE
+# num = int(input("Enter a number: "))
+
+# if num > 0:
+#     print("Number is Positive")
+# elif num < 0:
+#     print("Number is Negative")
+# else:
+#     print("Number is Zero")
+
+
+#Ask for two number and print their average
+
+# num1 = int(input("Enter num1: "))
+# num2 = int(input("Enter num2: "))
+
+# avg = (num1 + num2) / 2
+# print("Average is:", avg)
